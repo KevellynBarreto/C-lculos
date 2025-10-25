a99c8e41f5566ead86368759f9d34eacf3cafc06
Kevellyn Barreto Silva <kbarretosilva12@gmail.com>
2025-10-25 13:44:23 -0300

    Delete Cálculos directory

--- Cálculos/script2.js
+++
@@ -1,7 +0,0 @@
-function Mostrar (){
-    let raio = parseInt(document.querySelector(".valor-raio").value);
-    let area = Math.PI.toFixed(5) * Math.pow(raio,2);
-
-    document.querySelector(".resultado").innerHTML = area
-    console.log(area)
-}
--- Cálculos/script4.js
+++
@@ -1,7 +0,0 @@
-function Mostrar(){
-    let A = parseInt(document.querySelector(".valor-1").value);
-    let B = parseInt(document.querySelector(".valor-2").value);
-
-
-    document.querySelector(".valor").innerHTML = A * B;
-}
