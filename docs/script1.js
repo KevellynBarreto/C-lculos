cd1f980be866df0263de11e4189a3f2daafe436c
Kevellyn Barreto Silva <kbarretosilva12@gmail.com>
2025-10-25 13:46:14 -0300

    Add files via upload

---
+++ docs/script1.js
@@ -0,0 +1,22 @@
+/*var A = parseInt(prompt());
+let B = parseInt(prompt());
+
+var X = A + B; 
+
+ 
+
+console.log("X = "+ X);
+*/
+
+
+
+
+ function Mostrar(){
+
+    var valorA = parseInt(document.querySelector('.Pvalor-a').value) 
+    var valorB = parseInt(document.querySelector('.Pvalor-b').value)
+    var X = valorA + valorB;
+    document.querySelector('.valor').innerHTML = "O resultado é: " + X
+    console.log(valorA + valorB);
+    
+}
